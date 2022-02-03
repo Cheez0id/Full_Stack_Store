c8335e7e0ae4524a2369ad60f5c1e30bde43095a
Triston Golden <triston_golden@yahoo.com>
2022-02-03 14:31:10 -0500

    html to handlebars

--- Public/assets/js/index.js
+++ Public/assets/js/index.js
@@ -1,6 +1,7 @@
+//Mobiel Functionality
 const burgerMenu = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 
 burgerMenu.addEventListener("click", () => {
-  navbarMenu.classlist.toggle("is-active");
+  navbarMenu.classList.toggle("is-active");
 });
