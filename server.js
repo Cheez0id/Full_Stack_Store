12bfc2ff0c54b35af2ec6f924d75cb7968684a8c
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-03 11:05:56 -0500

    created server.js and updated connection.js

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const models = require('./models');
-//const category = require('./models/Product');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2');
 
+var fullstack_db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-// var commerce_db = mysql.createConnection({
-//   host: "127.0.0.1",
-//   user: process.env.DB_MARY,
-//   password: process.env.DB_PW,
-//   database: process.env.DB_SRC,
-// });
-
-// commerce_db.connect((err) => {
-//   if (err){
-//     throw err;
-//   }
-//   console.log('mysql is connected!')
-// })
+fullstack_db.connect((err) => {
+  if (err){
+    throw err;
+  }
+  console.log('mysql is connected!')
+})
 
 
 //username/passwords hidden via sequelize using .env 
