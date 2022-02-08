5e5a7045f192b8e5e6b2062071807dc4a4ec6665
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-07 21:40:59 -0500

    aligning the front end input field and seeds to the db format

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,15 +6,30 @@ burgerMenu.addEventListener("click", () => {
   navbarMenu.classList.toggle("is-active");
 });
 
-//modal
-const loginButton = document.querySelector("#login");
+
+const signUpButton = document.querySelector("#signUp");
 const modalBg = document.querySelector(".modal-background");
 const modal = document.querySelector(".modal");
 
-loginButton.addEventListener("click", () => {
+signUpButton.addEventListener("click", () => {
   modal.classList.add("is-active");
 });
 
 modalBg.addEventListener("click", () => {
   modal.classList.remove("is-active");
 });
+
+const registerButton = document.querySelector("#registerButton");
+
+// registerButton.addEventListener("click", () =>{
+//   //the api url; what you would put into insomnia if you wanted to post to the db
+//   fetch(url, {
+//     method: 'POST', 
+//       //an object with the information (email password,etc)
+//     body: <your post body>
+//   });
+
+// }
+// )
+
+
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,11 +2,11 @@ const { User } = require("../models");
 
 const userData = [
   {
-    user_name: "Fake",
-    user_id: 1,
-    address: "23 Bee",
-    phone: 4042252255,
-    payment_method:25,
+    id: 1,
+    email: "janeway@captain.com",
+    address: "USS ENTERPRISE",
+    password:"25",
+    paymentMethod:"25",
   },
 ];
 const seedUsers = () => User.bulkCreate(userData);
